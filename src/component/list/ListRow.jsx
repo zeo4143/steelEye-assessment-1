94526c3574d1dd4f2d4dc769de8d4df7ebc72ebd
Shaik Mahammad Rafi <117294084+zeo4143@users.noreply.github.com>
2023-08-04 16:11:08 +0530

    Update ListRow.jsx

--- src/component/list/ListRow.jsx
+++ src/component/list/ListRow.jsx
@@ -1,5 +1,5 @@
 import styles from "./ListRow.module.css";
-
+//props(item, index) from its grand parent Dashboard 
 const ListCell = ({ children, item, index }) => {
   return <tr className={styles.cell} onClick={() => item(index)}>{children}</tr>;
 };
